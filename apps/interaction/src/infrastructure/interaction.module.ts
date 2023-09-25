75b6a6bc02f52c18736f47c5674571ba7580ca63
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-09-25 22:31:20 +0200

    mongodb url config in interactions

--- apps/interaction/src/infrastructure/interaction.module.ts
+++ apps/interaction/src/infrastructure/interaction.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { InteractionController } from '../application/interaction.controller';
 import { InteractionService } from '../domain/interaction.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from '@friends-club/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      // isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/interaction/.env'
+    }),
+    DatabaseModule
+  ],
   controllers: [InteractionController],
   providers: [InteractionService],
 })
