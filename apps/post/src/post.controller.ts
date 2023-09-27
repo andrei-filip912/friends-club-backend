a3ade6da7b08a1f10e4ca9b6081fd7693aded795
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-09-27 22:03:34 +0200

    emit event from post service
    added mock controller post

--- apps/post/src/post.controller.ts
+++ apps/post/src/post.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { PostService } from './post.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class PostController {
   @Get()
   getHello(): string {
     return this.postService.getHello();
+  }
+
+  @Post()
+  createPost() {
+    return this.postService.createPost();
   }
 }
