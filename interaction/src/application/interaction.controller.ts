d7b7d7fbfc5550ff60595f4d00bb1f5ad0ea926a
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-10-19 06:46:47 +0200

    copied interaction files, added dependecies

---
+++ interaction/src/application/interaction.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get } from '@nestjs/common';
+import { InteractionService } from '../domain/interaction.service';
+import { Ctx, EventPattern, Payload } from '@nestjs/microservices';
+import { RmqContext } from '@nestjs/microservices';
+import { RmqService } from '@friends-club/common';
+
+@Controller()
+export class InteractionController {
+  constructor(
+    private readonly interactionService: InteractionService,
+    private readonly rmqService: RmqService
+    ) {}
+
+  @Get()
+  getHello(): string {
+    return this.interactionService.getHello();
+  }
+
+  @EventPattern('post_created')
+  async handleOrderCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+    this.interactionService.createInteraction(data);
+    this.rmqService.ack(context);
+  }
+}
---
+++ interaction/src/infrastructure/interaction.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { InteractionController } from '../application/interaction.controller';
+import { InteractionService } from '../domain/interaction.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from '@friends-club/common';
+import { ReactionRepository } from './reaction.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ReactionDocument, ReactionSchema } from './reaction.schema';
+import { RmqModule } from '@friends-club/common';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_INTERACTION_QUEUE: Joi.string().required(),
+      }),
+      envFilePath: './apps/interaction/.env'
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: ReactionDocument.name, schema: ReactionSchema}]),
+    RmqModule
+  ],
+  controllers: [InteractionController],
+  providers: [InteractionService, ReactionRepository],
+})
+export class InteractionModule {}
