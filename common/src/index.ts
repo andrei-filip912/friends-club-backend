f5d39d8c2a486fcd03b8f890ece419e44ba3083f
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-10-21 07:00:39 +0200

    fixed common library structure, importing from common works

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,4 +1,4 @@
-// steps: git commit, pnpm version patch, pnpm run build, pnpm publish --no-git-checks
+// steps: pnpm version patch, pnpm run build
 export * from './infrastructure/database/mongodb/mongodb.module';
 export * from './infrastructure/database/mongodb/mongodb.abstract.repository';
 export * from './infrastructure/database/mongodb/mongodb.abstract.schema';
@@ -6,3 +6,4 @@ export * from './infrastructure/services/rmq/rmq.service';
 export * from './domain/Interfaces/mongodb.interface.repository';
 export * from './domain/Interfaces/mongodb.interface.schema';
 export * from './infrastructure/services/rmq/rmq.module';
+export * from './domain/enums/reaction-type.enum';
