785218d25b01a6bd669e2092b08de8ea1b5c2c28
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-10-22 14:13:32 +0200

    ci test commit

--- apps/post/src/main.ts
+++ apps/post/src/main.ts
@@ -21,5 +21,6 @@ import { PostModule } from './post.module';
 async function bootstrap() {
   const app = await NestFactory.create(PostModule);
   await app.listen(3000);
+  console.log("test");
 }
 bootstrap();
