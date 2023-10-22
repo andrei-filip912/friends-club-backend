2ebc791dc75e7d4f187093b636ad21a69acd7895
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-10-22 15:48:27 +0200

    updated lint

--- apps/interaction/src/infrastructure/reaction.schema.ts
+++ apps/interaction/src/infrastructure/reaction.schema.ts
@@ -1,17 +1,17 @@
-import { AbstractDocument } from "@friends-club/common";
-import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
-import { ReactionType } from "@friends-club/common/domain/enums/reaction-type.enum";
+import { AbstractDocument } from '@friends-club/common';
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { ReactionType } from '@friends-club/common/domain/enums/reaction-type.enum';
 
-@Schema({ versionKey: false})
+@Schema({ versionKey: false })
 export class ReactionDocument extends AbstractDocument {
-    @Prop()
-    userId: string; // may change
+  @Prop()
+  userId: string; // may change
 
-    @Prop()
-    postId: string; // may change
+  @Prop()
+  postId: string; // may change
 
-    @Prop({ type: String, enum: ReactionType })
-    reactionType: ReactionType;
+  @Prop({ type: String, enum: ReactionType })
+  reactionType: ReactionType;
 }
 
 export const ReactionSchema = SchemaFactory.createForClass(ReactionDocument);
