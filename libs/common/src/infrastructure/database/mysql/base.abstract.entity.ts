5c4fd7fe04448541e2347508a98477a4124d829d
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-10-23 15:52:53 +0200

    updated base mysql entity with created at, updated at

--- libs/common/src/infrastructure/database/mysql/base.abstract.entity.ts
+++ libs/common/src/infrastructure/database/mysql/base.abstract.entity.ts
@@ -1,8 +1,22 @@
-import { PrimaryGeneratedColumn } from 'typeorm';
+import {
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 export class AbstractEntity<T> {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
 
   constructor(entity: Partial<T>) {
     Object.assign(this, entity);
