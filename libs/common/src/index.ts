11f6d7e9c84182b77c3450fb5eba4c8c6315e02a
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-10-23 15:14:04 +0200

    updated exports

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,5 +1,5 @@
 export * from './infrastructure/database/mongodb/mongodb.module';
-export * from './infrastructure/database/mongodb/mongodb.abstract.repository';
+export * from './infrastructure/database/mongodb/base.abstract.repository';
 export * from './infrastructure/database/mongodb/base.abstract.schema';
 export * from './infrastructure/services/rmq/rmq.service';
 export * from './domain/Interfaces/mongodb.interface.repository';
