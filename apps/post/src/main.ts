32550589172eb06e4fbb9c5b4f9d60f271bd5c41
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-10-30 19:38:31 +0100

    allowed cors to communicate to backend

--- apps/post/src/main.ts
+++ apps/post/src/main.ts
@@ -3,6 +3,10 @@ import { PostModule } from './infrastructure/post.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PostModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    // credentials: true,
+  });
   await app.listen(8000);
 }
 bootstrap();
