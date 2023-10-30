c227bddb61214f2e532306ccd8a9d86e5af331b1
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-10-30 19:27:58 +0100

    changed port to 8000

--- apps/post/src/main.ts
+++ apps/post/src/main.ts
@@ -3,6 +3,6 @@ import { PostModule } from './infrastructure/post.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PostModule);
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
