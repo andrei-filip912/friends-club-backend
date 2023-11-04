97ebba5a0b5a15420fa61dfdad91263681746329
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-11-04 23:02:05 +0100

    handling post image added rmq event in the upload service

--- apps/upload/src/upload.service.ts
+++ apps/upload/src/upload.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class UploadService {
+  private readonly logger = new Logger(UploadService.name);
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  uploadPostImage(data: any) {
+    this.logger.log('Image to be uploaded', data);
   }
 }
