974c968cff71d2e942b4b4a7463e89b950099c0c
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-11-06 11:41:36 +0100

    removed findOneAndReplaceById from base repo and interface in common

--- libs/common/src/infrastructure/database/mysql/base.abstract.repository.ts
+++ libs/common/src/infrastructure/database/mysql/base.abstract.repository.ts
@@ -1,5 +1,5 @@
 import { BaseInterfaceRepository } from '../../../domain/Interfaces/mysql.interface.repository';
-import { DeleteResult, FindOptionsWhere, ObjectLiteral, Repository } from 'typeorm';
+import { DeleteResult, FindOptionsWhere, Repository } from 'typeorm';
 import { AbstractEntity } from './base.abstract.entity';
 import { AggregateRoot } from '@nestjs/cqrs';
 import { EntityDbEntityFactory } from './entity-dbEntity.factory';
@@ -40,33 +40,31 @@ export abstract class BaseAbstractRepository<
     return entity;
   }
 
-  public async findOneAndReplaceById(
-    id: number,
-    replacement: TEntity,
-  ): Promise<TEntity> {
-    const dbEntity = await this.respository.findOne({
-      where: { id } as FindOptionsWhere<TDbEntity>,
-    });
+  // public async findOneAndReplaceById(
+  //   id: number,
+  //   replacement: TEntity,
+  // ): Promise<TEntity> {
+  //   const dbEntity = await this.respository.findOne({
+  //     where: { id } as FindOptionsWhere<TDbEntity>,
+  //   });
+  //   if (!dbEntity) {
+  //     throw new NotFoundException('Could no find the required post.');
+  //   }
+
+  //   // Update the entity in the database
+  //   this.respository.update(id, replacement as any);
+  //   return replacement;
+  // }
+
+  public async findByCondition(filterCondition: any): Promise<TEntity> {
+    const dbEntity = await this.respository.findOne({ where: filterCondition });
     if (!dbEntity) {
       throw new NotFoundException('Could no find the required post.');
     }
 
-    const replacementDbEntity = this.entityDbEntityFactory.create(replacement);
-
-    // Update the entity in the database
-    this.respository.update(id, replacementDbEntity);
-    return replacement;
-  }
-
-  public async findByCondition(filterCondition: any): Promise<TEntity | null> {
-    const dbEntity = await this.respository.findOne({ where: filterCondition });
-
     // Use your factory to create the domain entity
-    if (dbEntity) {
-      const entity = this.entityDbEntityFactory.createFromDbEntity(dbEntity);
-      return entity;
-    }
-    return null;
+    const entity = this.entityDbEntityFactory.createFromDbEntity(dbEntity);
+    return entity;
   }
 
   public async findWithRelations(relations: any): Promise<TEntity[]> {
