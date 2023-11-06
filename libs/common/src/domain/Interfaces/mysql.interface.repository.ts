f273ee6b25f464fdab3e4b4e37ea272ac0e99657
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-11-06 15:30:52 +0100

    updated remove function in mysql base repo class and interface

--- libs/common/src/domain/Interfaces/mysql.interface.repository.ts
+++ libs/common/src/domain/Interfaces/mysql.interface.repository.ts
@@ -11,7 +11,7 @@ export interface BaseInterfaceRepository<T> {
 
   findAll(): Promise<T[]>;
 
-  remove(id: string): Promise<DeleteResult>;
+  remove(id: number): Promise<DeleteResult>;
 
   findWithRelations(relations: any): Promise<T[]>;
 }
