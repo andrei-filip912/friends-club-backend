243574b46cc616a83a1731000b43cfdda8145596
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-11-22 15:11:04 +0100

    updated cors origin value, set api prefix

--- apps/post/src/main.ts
+++ apps/post/src/main.ts
@@ -4,9 +4,10 @@ import { PostModule } from './infrastructure/post.module';
 async function bootstrap() {
   const app = await NestFactory.create(PostModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://friends-club.dev:3000',
     // credentials: true,
   });
+  app.setGlobalPrefix('api');
   await app.listen(8000);
 }
 bootstrap();
