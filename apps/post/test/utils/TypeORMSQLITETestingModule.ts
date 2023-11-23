fa7e0cd6c34a0d2ea379c4e3bf31b83ef708cd25
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-11-23 20:50:28 +0100

    added e2e tests based on user stories
    updated tests config
    updated type orm mock module

--- apps/post/test/utils/TypeORMSQLITETestingModule.ts
+++ apps/post/test/utils/TypeORMSQLITETestingModule.ts
@@ -1,14 +1,18 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostDbEntity } from '../../src/infrastructure/post.db-entity';
 import { PostDto } from '../../src/application/dto/post.dto';
+import { Module } from '@nestjs/common';
 
-export const TypeOrmSQLITETestingModule = () => [
-  TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: ':memory:',
-    dropSchema: true,
-    entities: [PostDbEntity, PostDto],
-    synchronize: true,
-  }),
-  TypeOrmModule.forFeature([PostDbEntity, PostDto]),
-];
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      dropSchema: true,
+      entities: [PostDbEntity, PostDto],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([PostDbEntity, PostDto]),
+  ],
+})
+export class SqlMockModule {}
