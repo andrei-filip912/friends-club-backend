d578c04ce1b3a968919745604f67ff3dd96f9bc5
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2023-12-14 12:32:33 +0100

    renamed interaction sub folder to reaction

--- apps/interaction/src/main.ts
+++ apps/interaction/src/main.ts
@@ -5,6 +5,10 @@ import { RmqService } from '@friends-club/common';
 async function bootstrap() {
   const app = await NestFactory.create(InteractionModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'https://friends-club.dev:3000',
+    // credentials: true,
+  });
   const rmqService = app.get<RmqService>(RmqService);
   app.connectMicroservice(rmqService.getOptions('INTERACTION'));
   await app.startAllMicroservices();
--- apps/interaction/src/reaction/reaction.controller.ts
+++ apps/interaction/src/reaction/reaction.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
+import { AuthorizationGuard } from '@friends-club/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
-import { CreateReactionRequest } from './dto/create-reaction-request.dto';
 import { CreateReactionCommand } from './commands/create-reaction/create-reaction.command';
+import { CreateReactionRequest } from './dto/create-reaction-request.dto';
 
 @Controller('reaction')
+@UseGuards(AuthorizationGuard)
 export class ReactionController {
   constructor(private readonly commandBus: CommandBus) {}
 
@@ -15,8 +25,10 @@ export class ReactionController {
 
   @Post()
   async createReaction(
+    @Req() req: Request,
     @Body() createReactionRequest: CreateReactionRequest,
   ): Promise<void> {
+    console.log(req);
     await this.commandBus.execute<CreateReactionCommand, void>(
       new CreateReactionCommand(createReactionRequest),
     );
