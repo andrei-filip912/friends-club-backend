51616a36bb395dae2c556d645f08a4b4210b5df2
andrei-filip912 <79208621+andrei-filip912@users.noreply.github.com>
2024-01-12 15:09:44 +0100

    validating the env variables on start

--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_USER_QUEUE: Joi.string().required(),
+        AUTH0_AUDIENCE: Joi.string().required(),
+        AUTH0_DOMAIN: Joi.string().required(),
+      }),
+      envFilePath: './apps/user/.env.local',
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
